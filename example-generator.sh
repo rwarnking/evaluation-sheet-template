571f72b9a2d9601f3a5ef7bc3d0d5f102bc63654
rwarnking <rwarnking@gmail.com>
2022-03-29 14:58:29 +0200

    Add point-file generation and rm previous tgtdir

--- example-generator.sh
+++ example-generator.sh
@@ -17,6 +17,10 @@ function Add-Column {
 if [[ ! (-d ./$TgtFolder) ]]; then
     # Create data folder if not existent and hide console output
     mkdir -p ./$TgtFolder
+else
+    # Delete and recreate the folder
+    rm -r ./$TgtFolder
+    mkdir -p ./$TgtFolder
 fi
 
 ###################################################################################################
@@ -58,7 +62,7 @@ while IFS='' read -r line; do namenumbers+=("$line"); done < <(shuf -i 1-$TotalP
 } >> $FileName
 
 # Main loop iterating all participants
-for (( i=0; i<TotalParticipants; i++ ))
+for (( p=0; p<TotalParticipants; p++ ))
 do
     # Get random salutation
     index=$(( RANDOM % ${#salutationList[@]} ))
@@ -72,7 +76,7 @@ do
     index=$(( RANDOM % 20 + 1))
     fieldofstudy="$fieldofstudy, $index"
     # Add leading zeros to the namenumber
-    tmp=$(printf "%0*d" ${#TotalParticipants} "${namenumbers[$i]}")
+    tmp=$(printf "%0*d" ${#TotalParticipants} "${namenumbers[$p]}")
     # Print all columns of the current row
     Add-Column "$salutation"
     Add-Column "$title"
@@ -85,7 +89,7 @@ do
     Add-Column "${lastname}${tmp}$email"
     Add-Column "$enroldate"
     Add-Column "$fieldofstudy"
-    Add-Column "${ids[$i]}"
+    Add-Column "${ids[$p]}"
     Add-Column "$misc"
     printf "\n" >> $FileName
 done
@@ -99,7 +103,7 @@ printf "tutorid; groupid; date; participants; droppedout;\n" >> $FileName
 # Create an arbitrary number of group with a random groupsize of upto four
 members=0
 groupList=()
-for (( i=0; i<TotalParticipants; i+=members ))
+for (( p=0; p<TotalParticipants; p+=members ))
 do
     rand=$(( RANDOM % 100 ))
     if [[ $rand -lt 10 ]]; then
@@ -134,25 +138,26 @@ while IFS='' read -r line; do idsidx+=("$line"); done < <(shuf -i 0-${length} -n
 # Add a row to the csv for each group
 start=0
 end=0
-for (( i=0; i<${#groupList[@]}; i++ ))
+for (( g=0; g<${#groupList[@]}; g++ ))
 do
     # Assign a random tutornumber
     tutor=$(( RANDOM % tutorcount + 1 ))
     Add-Column "$tutor"
     # Add groupnumber
-    Add-Column "$i"
+    group=$((g+1))
+    Add-Column "$group"
     # Add date for the group, the dates are sorted
-    idx=${dateidx[$i]}
+    idx=${dateidx[$g]}
     Add-Column "${dateList[$idx]}"
 
     # For each participant of this group add the part-number and the drop-number
     partarray=""
     droparray=""
     start=$end
-    ((end+=${groupList[$i]}))
-    for (( j=start; j<end; j++ ))
+    ((end+=${groupList[$g]}))
+    for (( i=start; i<end; i++ ))
     do
-        idx=${idsidx[$j]}
+        idx=${idsidx[$i]}
         partarray+="${ids[$idx]}"
         # Add a random number to indicate if the participant droped out and during which sheet
         rand=$(( RANDOM % 100 ))
@@ -166,7 +171,7 @@ do
             # Participant did not drop out and was part of this group from the beginning
             droparray+="0"
         fi
-        if [[ $j -lt $end-1 ]]; then
+        if [[ $i -lt $end-1 ]]; then
             partarray+=","
             droparray+=","
         fi
@@ -190,6 +195,7 @@ done
 #######################################
 function Add-Subtaskrow {
     Add-Column "$ipo${letters[$idx]}$1"
+    taskIdList+=("$ipo${letters[$idx]}$1")
     # Title column is empty for all subtasks
     if [[ $taskcols -eq 1 ]]; then
         Add-Column ""
@@ -236,8 +242,14 @@ do
     subtasks=0
     # List of integers specifying the number of subtasks for each task
     taskList=()
+    # List of all task ids / points, used for point sheet
+    taskIdList=()
+    pointsList=()
+    subpointsList=()
+    # Max points available on this sheet (starts with 1 for %, used for point sheet)
+    sheetPoints=1
     # Fill the tasklist with pseudo random values
-    for (( i=0; i<TotalTasks; i+=subtasks ))
+    for (( t=0; t<TotalTasks; t+=subtasks ))
     do
         rand=$(( RANDOM % 100 ))
         if [[ $rand -lt 30 ]]; then
@@ -253,33 +265,38 @@ do
     done
 
     # Generate each task with the definied number of subtasks
-    for (( i=0; i<${#taskList[@]}; i++ ))
+    for (( t=0; t<${#taskList[@]}; t++ ))
     do
         # List of integers specifying the points for each subtasks
         subpoints=()
-        while IFS='' read -r line; do subpoints+=("$line"); done < <(shuf -i 1-4 -n "${taskList[i]}" -r)
+        while IFS='' read -r line; do subpoints+=("$line"); done < <(shuf -i 1-4 -n "${taskList[t]}" -r)
 
         # Sum the subtaskpoints
         sumpoints=0
         for val in "${subpoints[@]}"; do
             ((sumpoints+=val))
         done
+        # Add all point values to one list
+        pointsList+=($sumpoints)
+        subpointsList+=(${subpoints[@]})
+        (( sheetPoints+=$sumpoints ))
 
         # Add columns for taskid, title and points
-        ipo=$((i+1))
+        ipo=$((t+1))
         Add-Column "$ipo"
+        taskIdList+=("$ipo")
         Add-Column "Assignment $ipo"
         Add-Column "$sumpoints"
         # Add the description column with value dependent on the column type
         # and the number of subtasks.
         if [[ $taskcols -eq 1 ]]; then
-            if [[ ${taskList[i]} -gt 1 ]]; then
+            if [[ ${taskList[t]} -gt 1 ]]; then
                 Add-Column "Sum"
             else
                 Add-Column "Result"
             fi
         else
-            if [[ ${taskList[i]} -gt 1 ]]; then
+            if [[ ${taskList[t]} -gt 1 ]]; then
                 Add-Column ""
             else
                 # Select a random predefined description
@@ -290,22 +307,24 @@ do
         printf "\n" >> $FileName
 
         # Add subtasks if there are any
-        if [[ ${taskList[i]} -gt 1 ]]; then
+        if [[ ${taskList[t]} -gt 1 ]]; then
             idx=0
-            for (( j=0; j<${taskList[$i]}; j++ ))
+            for (( j=0; j<${taskList[$t]}; j++ ))
             do
                 # If taskcolumns are enabled randomly use the -1, ... subfix
+                # But check for max amount of subtasks
                 if [[ $taskcols -eq 1 ]]; then
                     rand=$(( RANDOM % 100 ))
-                    if [[ $rand -lt 15 ]]; then
+                    if [[ $rand -lt 30 && $((j+2))<${taskList[$t]} ]]; then
                         Add-Subtaskrow "-1"
-                        Add-Subtaskrow "-2"
-                        Add-Subtaskrow "-3"
-                        ((j+=2))
-                    elif [[ $rand -lt 30 ]]; then
-                        Add-Subtaskrow "-1"
+                        ((j++))
                         Add-Subtaskrow "-2"
                         ((j++))
+                        Add-Subtaskrow "-3"
+                    elif [[ $rand -lt 50 && $((j+1))<${taskList[$t]} ]]; then
+                        Add-Subtaskrow "-1"
+                        ((j++))
+                        Add-Subtaskrow "-2"
                     else
                         Add-Subtaskrow
                     fi
@@ -316,13 +335,92 @@ do
             done
         fi
     done
+
+    ###############################################################################################
+    # sheet_01_points.csv
+    ###############################################################################################
+    # Create name and push header to the file
+    FileName="./${TgtFolder}/sheet_0${s}_points.csv"
+    Add-Column "groupid"
+    # Add header
+    for (( t=0; t<${#taskIdList[@]}; t++ ))
+    do
+        Add-Column "${taskIdList[t]}"
+    done
+    printf "\n" >> $FileName
+
+    # Add all groups
+    for (( g=1; g<=${#groupList[@]}; g++ ))
+    do
+        # Add group id
+        Add-Column "$g"
+        # Add random points for the complete sheet
+        totalpoints=$(( RANDOM % $sheetPoints ))
+        taskpoints=()
+        notfull=()
+
+        # Initialize the taskpoints with zero
+        for (( t=0; t<${#taskList[@]}; t++ ))
+        do
+            taskpoints+=(0)
+            notfull+=($t)
+        done
+
+        # Distribute all points in increments of one to the different tasks
+        # If a task is full remove it from the list such that it can not get any more points
+        for (( p=0; p<$totalpoints; p++ ))
+        do
+            # Get random task idx which is not yet full
+            idx=$(( RANDOM % ${#notfull[@]} ))
+            # Get element at idx and increment
+            tidx=${notfull[$idx]}
+            (( taskpoints[$tidx]++ ))
+            # Remove from list
+            if [[ ${taskpoints[$tidx]} -eq ${pointsList[$tidx]} ]]; then
+                unset "notfull[$idx]"
+                notfull=("${notfull[@]}")
+            fi
+        done
+
+        # offset needed to access the correct point informations for the subtasks
+        offset=0
+        # Iterate all major tasks
+        for (( t=0; t<${#taskList[@]}; t++ ))
+        do
+            # Add points for the current task
+            Add-Column "${taskpoints[t]}"
+            # Process potential subtasks
+            if [[ ${taskList[t]} -gt 1 ]]; then
+                subtaskpoints=()
+                notfull=()
+                # Initialize the subtaskpoints with zero
+                for (( st=0; st<${taskList[t]}; st++ ))
+                do
+                    subtaskpoints+=(0)
+                    notfull+=($st)
+                done
+                # Distribute the points for this task across all subtasks
+                for (( p=0; p<${taskpoints[t]}; p++ ))
+                do
+                    # Get random task idx which is not yet full
+                    idx=$(( RANDOM % ${#notfull[@]} ))
+                    # Get element at idx and increment
+                    stidx=${notfull[$idx]}
+                    (( subtaskpoints[$stidx]++ ))
+                    # Remove from list (beware the offset)
+                    if [[ ${subtaskpoints[$stidx]} -eq ${subpointsList[$((stidx+offset))]} ]]; then
+                        unset "notfull[$idx]"
+                        notfull=("${notfull[@]}")
+                    fi
+                done
+                # Add the point value to the column
+                for (( st=0; st<${taskList[t]}; st++ ))
+                do
+                    Add-Column "${subtaskpoints[st]}"
+                done
+            fi
+            ((offset+=${taskList[t]}))
+        done
+        printf "\n" >> $FileName
+    done
 done
-
-###################################################################################################
-# sheet_01_points.csv
-###################################################################################################
-# TODO
-# Generate random points file
-# Add linter for bash and powershell
-
-# $SHELL
